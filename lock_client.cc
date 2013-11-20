ddea86bcf5de9ff7903457a700995e8349f24528
Daniel Zhang <actzendaria@gmail.com>
2013-11-21 01:23:03 +0800

    Temp1.

--- lock_client.cc
+++ lock_client.cc
@@ -7,6 +7,7 @@
 #include <sstream>
 #include <iostream>
 #include <stdio.h>
+#include <pthread.h>
 
 lock_client::lock_client(std::string dst)
 {
@@ -30,10 +31,18 @@ lock_client::stat(lock_protocol::lockid_t lid)
 lock_protocol::status
 lock_client::acquire(lock_protocol::lockid_t lid)
 {
+  int r;
+  lock_protocol::status ret = cl->call(lock_protocol::acquire, cl->id(), lid, r);
+  VERIFY (ret == lock_protocol::OK);
+  return r;
 }
 
 lock_protocol::status
 lock_client::release(lock_protocol::lockid_t lid)
 {
+  int r;
+  lock_protocol::status ret = cl->call(lock_protocol::release, cl->id(), lid, r);
+  VERIFY (ret == lock_protocol::OK);
+  return r;
 }
 
--- lock_server.cc
+++ lock_server.cc
@@ -20,4 +20,62 @@ lock_server::stat(int clt, lock_protocol::lockid_t lid, int &r)
   return ret;
 }
 
+lock_protocol::status
+lock_server::acquire(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  lock_protocol::status ret = lock_protocol::OK;
+  printf("acq request from clt %d\n", clt);
 
+  // use a simple spin-like block if locked
+  std::map<lock_protocol::lockid_t, lstatus>::iterator it;
+  if ( (it = llock.find(lid)) != llock.end() ) {
+    // lock exist
+    while(true) {
+      if (it->second == FREE) {
+        it->second = LOCKED;
+        r = nacquire;
+        return ret;
+        break;
+      }
+    }
+  }
+  else { // create & lock
+    llock.insert(std::pair<lock_protocol::lockid_t, lstatus>(lid, LOCKED));
+    r = nacquire;
+    return ret;
+  }
+
+  r = nacquire;
+  ret = lock_protocol::RETRY;
+  return ret;
+}
+
+lock_protocol::status
+lock_server::release(int clt, lock_protocol::lockid_t lid, int &r)
+{
+  lock_protocol::status ret = lock_protocol::OK;
+  printf("rls request from clt %d\n", clt);
+  std::map<lock_protocol::lockid_t, lstatus>::iterator it;
+  if ( (it = llock.find(lid)) != llock.end() ) {
+    // lock exist
+    if (it->second == LOCKED) {
+      it->second = FREE;
+      r = nacquire;
+      return ret;
+    }
+    else {
+      r = nacquire;
+      ret = lock_protocol::NOENT;
+      return ret;
+    }
+  }
+  else {
+    r = nacquire;
+    ret = lock_protocol::NOENT;
+    return ret;
+  }
+
+  r = nacquire;
+  ret = lock_protocol::RETRY;
+  return ret;
+}
