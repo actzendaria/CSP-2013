cb08993f022cc37c5f6b2e4286cef9aca8fa2ff2
Daniel Zhang <actzendaria@gmail.com>
2013-12-13 20:02:28 +0800

    temp

--- extent_client.cc
+++ extent_client.cc
@@ -7,6 +7,8 @@
 #include <unistd.h>
 #include <time.h>
 
+#define REMOVE_WRITE_BACk 1
+
 extent_client::extent_client(std::string dst)
 {
   sockaddr_in dstsock;
@@ -15,6 +17,12 @@ extent_client::extent_client(std::string dst)
   if (cl->bind() != 0) {
     printf("extent_client: bind failed\n");
   }
+  VERIFY(pthread_mutex_init(&mx, NULL) == 0);
+}
+
+extent_client::~extent_client()
+{
+  printf("extent_client: destroying..\n");
 }
 
 // a demo to show how to use RPC
@@ -23,6 +31,7 @@ extent_client::getattr(extent_protocol::extentid_t eid,
 		       extent_protocol::attr &attr)
 {
   extent_protocol::status ret = extent_protocol::OK;
+  pthread_mutex_lock(&mx);
   if (extent_cache.find(eid) == extent_cache.end()) {
     extent_cache[eid].dirty = false;
     extent_cache[eid].valid_buf = false;
@@ -40,6 +49,7 @@ extent_client::getattr(extent_protocol::extentid_t eid,
     //attr is cached~
     attr = extent_cache[eid].attr;
   }
+  pthread_mutex_unlock(&mx);
 
   printf("zzz: ec: getattr eid(%llu)\n", eid);
   return ret;
@@ -50,6 +60,7 @@ extent_client::create(uint32_t type, extent_protocol::extentid_t &id)
 {
   extent_protocol::status ret = extent_protocol::OK;
   ret = cl->call(extent_protocol::create, type, id);
+  pthread_mutex_lock(&mx);
   if (extent_cache.find(id) == extent_cache.end()) {
     extent_cache[id].dirty = false;
     extent_cache[id].valid_buf = false;
@@ -69,6 +80,8 @@ extent_client::create(uint32_t type, extent_protocol::extentid_t &id)
   extent_cache[id].valid_attr = true;
   extent_cache[id].valid_buf = true;
 
+  pthread_mutex_unlock(&mx);
+
   printf("zzz: ec: create eid(%llu) type(%u)\n", id, type);
   // Your lab3 code goes here
   return ret;
@@ -78,6 +91,7 @@ extent_protocol::status
 extent_client::get(extent_protocol::extentid_t eid, std::string &buf)
 {
   extent_protocol::status ret = extent_protocol::OK;
+  pthread_mutex_lock(&mx);
   if (extent_cache.find(eid) == extent_cache.end()) {
     extent_cache[eid].dirty = false;
     extent_cache[eid].valid_buf = false;
@@ -97,6 +111,7 @@ extent_client::get(extent_protocol::extentid_t eid, std::string &buf)
     time_t tm = time(NULL);
     extent_cache[eid].attr.atime = (uint32_t)tm;
   }
+  pthread_mutex_unlock(&mx);
   printf("ec get(%llu) get_buf_sz(%u)\n", eid, buf.size());
   // Your lab3 code goes here
   return ret;
@@ -106,6 +121,7 @@ extent_protocol::status
 extent_client::put(extent_protocol::extentid_t eid, std::string buf)
 {
   extent_protocol::status ret = extent_protocol::OK;
+  pthread_mutex_lock(&mx);
   //int tmp;
   if (extent_cache.find(eid) == extent_cache.end()) {
     extent_cache[eid].dirty = false;
@@ -141,6 +157,7 @@ extent_client::put(extent_protocol::extentid_t eid, std::string buf)
   extent_cache[eid].valid_buf = true;
   extent_cache[eid].valid_attr = true;
   
+  pthread_mutex_unlock(&mx);
 
   printf("ec put(%llu) strsz(%u)%s\n", eid, buf.size(), buf.c_str());
   //ret = cl->call(extent_protocol::put, eid, buf, tmp);
@@ -152,8 +169,14 @@ extent_protocol::status
 extent_client::remove(extent_protocol::extentid_t eid)
 {
   extent_protocol::status ret = extent_protocol::OK;
-  //int tmp;
+  pthread_mutex_lock(&mx);
 
+#ifdef REMOVE_WRITE_BACK
+  printf("ZZZ REMOVE HELLO!\n");
+  int tmp;
+  extent_cache.erase(eid);
+  ret = cl->call(extent_protocol::remove, eid, tmp);
+#else
   // cache for eid is no longer valid since it is removed
   extent_cache[eid].valid_buf = false;
   extent_cache[eid].valid_attr = false;
@@ -162,12 +185,65 @@ extent_client::remove(extent_protocol::extentid_t eid)
   extent_cache[eid].attr.atime = 0;
   extent_cache[eid].attr.mtime = 0;
   extent_cache[eid].attr.ctime = 0;
-  extent_cache.erase(eid);
-
-  //ret = cl->call(extent_protocol::remove, eid, tmp);
+  extent_cache[eid].removed = true;
+  //extent_cache.erase(eid);
+#endif
+  pthread_mutex_unlock(&mx);
   printf("zzz: ec: remove eid(%llu)\n", eid);
   // Your lab3 code goes here
   return ret;
 }
 
+extent_protocol::status
+extent_client::flush(extent_protocol::extentid_t eid)
+{
+  extent_protocol::status ret = extent_protocol::OK;
+  std::string buf;
+  int tmp;
+  pthread_mutex_lock(&mx);
+#ifdef REMOVE_WRITE_BACK
+  // if eid is removed, cannot find eid since it was erased
+  if (extent_cache.find(eid) == extent_cache.end()) {
+    // removed or wrong eid input
+    pthread_mutex_unlock(&mx);
+    return ret;
+  }
 
+  if (extent_cache[eid].dirty) {
+    buf = extent_cache[eid].buf;
+    ret = cl->call(extent_protocol::put, eid, buf, tmp);
+    printf("ec flush(%llu), dirty ask server put RPC...\n", eid);
+  }
+  else {
+    printf("ec flush(%llu), REMOVE WRITE_BACK not dirty, no RPC call...\n", eid);
+    // no need to put back to server
+  }
+#else
+  if (extent_cache.find(eid) == extent_cache.end()) {
+    printf("ec flush(%llu), first time, unknown eid!\n", eid);
+    pthread_mutex_unlock(&mx);
+    ret = extent_protocol::NOENT;
+    return ret;
+  }
+
+  if (extent_cache[eid].removed) {
+    ret = cl->call(extent_protocol::remove, eid, tmp);
+    printf("ec flush(%llu), removed ask server remove RPC...\n", eid);
+  }
+  else if (extent_cache[eid].dirty) {
+    buf = extent_cache[eid].buf;
+    ret = cl->call(extent_protocol::put, eid, buf, tmp);
+    printf("ec flush(%llu), dirty ask server put RPC...\n", eid);
+  }
+  else {
+    printf("ec flush(%llu), neither removed nor dirty, no RPC call...\n", eid);
+    // no need to put back to server
+  }
+  // disable eid's local cache
+  extent_cache.erase(eid);
+#endif
+
+  pthread_mutex_unlock(&mx);
+  printf("ec: flush eid(%llu)\n", eid);
+  return ret;
+}
